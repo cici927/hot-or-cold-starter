38038f3192b833630f040f433055e6c68610b8ac
Celia Tomas <cici_927@hotmail.com>
2016-07-26 17:45:32 -0400

    commit message

--- js/app2.js
+++ js/app2.js
@@ -4,13 +4,14 @@ $(document).ready(function() {
 	var countGuess = 0;
 	var checkGuess;
 
-
 	function newGame() {
 		randNum = Math.floor((Math.random() * 100) + 1);
 		console.log(randNum);
 		countGuess = 0;
+		$("#userGuess").val("");
 		$("#count").text(countGuess);
 		$("#feedback").text("Make your Guess!");
+		$("#guessList").html("");
 	}
 	
 	$(".new").click(function(event){
@@ -20,51 +21,42 @@ $(document).ready(function() {
 
 	newGame();
 
-
 	$("#guessButton").click(function(e) {
 		e.preventDefault();
 		var userGuess = $("#userGuess").val();
 		userGuess = +userGuess;
+		$("#userGuess").val("");
 		if(Math.floor(userGuess) == userGuess && userGuess > 0 && userGuess < 101) {
-		console.log(userGuess);
-	}
-	else {
-		alert("Try a number between 1 and 100!")
-	}
+			checkGuess = Math.abs(userGuess - randNum);
+			howCloseToNum(checkGuess);
+			$("#guessList").append("<li>" + userGuess + "</li>");
+			countGuess++;
+			$("#count").text(countGuess);
+		}
+		else {
+			alert("Try a number between 1 and 100!")
+		}
 
-	checkGuess = Math.abs(userGuess - randNum);
-	howCloseToNum(checkGuess);
-
-	$("#guessList").append(numGuess);
-
-	countGuess++;
-	$("#count").text(countGuess);
-		console.log(countGuess);
 	})
 
 	function howCloseToNum(checkGuess) {
 
 		if (checkGuess == 0) {
 			numFeedback("You won! Great guess!");
-			}
+		}
 	 	else if (checkGuess < 5) {
 			numFeedback("Your guess is heating up!");
-			
-	 	}
+		}
 	 	else if (checkGuess < 20) {
 			numFeedback("Getting kinda warm!");
-			
-	 	}
+		}
 	 	else if (checkGuess < 30) {
 			numFeedback("Not warm enough! Keep trying!");
-			
-	 	}
+		}
 	 	else {
 			numFeedback("Burr!! You're super cold!");
-			
-	 	}
-
-	 }
+		}
+	}
 
 	 function numFeedback(feedback) {
 	 	$("#feedback").text(feedback);
